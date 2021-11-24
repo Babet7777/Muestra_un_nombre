33791ebd0103acec3e8286c617a87f19f75c1fc2
Babet7777 <94986798+Babet7777@users.noreply.github.com>
2021-11-24 18:36:45 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,3 +2,10 @@ basic.forever(function () {
     basic.showString("Fran")
     basic.pause(1000)
 })
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showString("Borrego")
+        basic.pause(1000)
+        basic.clearScreen()
+    }
+})
